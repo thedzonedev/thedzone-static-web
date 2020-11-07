b734a32425fc42433a8814df1bb449454a35fef0
thedzonedev <thedzonedev@gmail.com>
2020-11-07 13:53:54 +0530

    setup

---
+++ staticmain/src/components/MainScreen/FullMainScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FullMainScreen extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
