af3097bb3f8d68a2d6d3469083c7850342b422e2
thedzonedev <thedzonedev@gmail.com>
2020-11-09 22:27:33 +0530

    Changes for the Main screen page and Header

--- staticmain/src/components/Common/Header.js
+++ staticmain/src/components/Common/Header.js
@@ -1,14 +1,160 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
 
-export default class Header extends Component {
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
+const useStyles = makeStyles((theme) => ({
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    sectitle: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+        margin: theme.spacing(2)
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(1),
+        height: '100%',
+        position: 'relative',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+    sectionMobile: {
+        display: 'flex',
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
+    backcolor: {
+        backgroundColor: '#3D80E4',
+        padding: theme.spacing(3)
     }
+}));
+
+export default function Header() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    return (
+        <div className={classes.grow}>
+            <AppBar position="static">
+                <Toolbar className={classes.backcolor}>
+                    <Typography className={classes.title} variant="h4" noWrap>
+                        CANVAS
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.sectionDesktop}>
+                        <Typography className={classes.sectitle} variant="h10" noWrap>
+                            Home
+                        </Typography>
+                        <Typography className={classes.sectitle} variant="h10" noWrap>
+                            Features
+                        </Typography>
+                        <Typography className={classes.sectitle} variant="h10" noWrap>
+                            About
+                        </Typography>
+                        <Typography className={classes.sectitle} variant="h10" noWrap>
+                            Pricing
+                        </Typography>
+                        <Typography className={classes.sectitle} variant="h10" noWrap>
+                            Contact
+                        </Typography>
+                    </div>
+                    <div className={classes.sectionMobile}>
+                        <Typography variant="h6" className={classes.searchIcon} noWrap>
+                            CANVAS
+                        </Typography>
+                        <IconButton
+                            aria-label="show more"
+                            aria-haspopup="true"
+                            onClick={handleMobileMenuOpen}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
-
-
-
--- staticmain/src/components/MainScreen/FullMainScreen.js
+++ staticmain/src/components/MainScreen/FullMainScreen.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import { fade, makeStyles } from '@material-ui/core/styles';
 
-export default class FullMainScreen extends Component {
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
+const useStyles = makeStyles((theme) => ({
+    paddingForTitle: {
+        padding: theme.spacing(1)
+    },
+    fontSizeForDesc: {
+        fontSize: '1.2em',
+        padding: theme.spacing(0, 4),
+        color: 'rgba(0,0,0,0.8)'
     }
+}));
+
+export default function FullMainScreen() {
+    const classes = useStyles()
+    return (
+        <Container>
+            <Row>
+                <Col md={{ span: 6, offset: 3 }}>
+                    <h6 style={{ fontWeight: "bold", color: "grey" }}>WHY YOU CHOOSE US?</h6>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={{ span: 8, offset: 2 }} className={classes.paddingForTitle}>
+                    <p style={{ fontSize: "3em", fontWeight: "bold" }}>Powerful & Best Features for Canvas App</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={{ span: 8, offset: 2 }} className={classes.fontSizeForDesc} >
+                    Dynamically underwhelm end-to-end experiences for premier methodologies. Objectively benchmark front-end bandwidth vis-a-vis flexible e-services. Globally drive business convergence without backward-compatible markets.
+                </Col>
+            </Row>
+        </Container>
+    );
 }
